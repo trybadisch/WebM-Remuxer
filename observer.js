6051a2e7d2eab7f8455b356c963d2a4ba9d14f4e
trybadisch <76852449+trybadisch@users.noreply.github.com>
2025-08-07 09:10:48 +0200

    Delete observer.js

--- observer.js
+++
@@ -1,9 +0,0 @@
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-      for (let i = 0; i < mutation.addedNodes.length; i++) {
-        const newNode = mutation.addedNodes[i];
-      }
-    }
-  });
-});
